ce3962acdd8d1e620b41f51df90bd9f9eae74565
Swiss-droid <njabulogama82@gmail.com>
2026-07-14 17:52:42 +0200

    Add files via upload

---
+++ The Concert Ticket Booker.py
@@ -0,0 +1,7 @@
+print("<=====The Concert Ticket Booker=====>")
+
+name = input("Enter your name: ")
+artist = input("Enter the artist/band that you want to see: ")
+confirmation = f"\nHey {name}! Your tickets to see {artist} are booked successfully!"
+
+print(confirmation)
