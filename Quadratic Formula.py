4e8d40f55a477abcc778e59a43a36eddc5712d07
Swiss-droid <njabulogama82@gmail.com>
2026-09-25 15:48:05 +0200

    Add files via upload

---
+++ Quadratic Formula.py
@@ -0,0 +1,22 @@
+import math
+
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+c = float(input("Enter the value of c: "))
+
+part_1 = math.sqrt(-b + (b ** 2) - 4 * (a * c))
+part_2 = 2 * a
+part_3 = part_1 / part_2
+
+part_4 = math.sqrt(-b - (b ** 2) - 4 * (a * c))
+part_2 = 2 * a
+part_5 = part_4 / part_2
+
+print(part_3)
+print(part_5)
+
+#x1 = math.sqrt(-b + (b ** 2) - (4 * a * c)) / 2 * a 
+#x2 = math.sqrt(-b - (b ** 2) - (4 * a * c)) / 2 * a
+
+#print(x1)
+#print(x2)
